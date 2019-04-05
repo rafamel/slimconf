c5d25f5dd5d78e7c5ed2679ecc635988e992ac34
Rafa Mel <melrcm@gmx.net>
2019-04-05 14:04:40 +0200

    feat(strategies): prevents mutations from having an effect over defaults

--- src/strategies.ts
+++ src/strategies.ts
@@ -1,31 +1,37 @@
 import mergewith from 'lodash.mergewith';
+import clonedeep from 'lodash.clonedeep';
 
 /**
- * If both the default value and the value for the environment are objects, they will be shallow merged.
+ * If both `defaults` and `value` are objects, they will be shallow merged. Mutations to the returned object won't have an effect over `defaults`.
  */
-export function shallow<T>(defaults: any, value: T): T {
+export function shallow<T, D>(defaults: D, value: T): T | (T & D) {
   return typeof value === 'object' &&
     !Array.isArray(value) &&
     typeof defaults === 'object' &&
     !Array.isArray(defaults)
-    ? Object.assign({}, defaults, value)
+    ? Object.assign(clonedeep(defaults), value)
     : value;
 }
 
 /**
- * If both the default value and the value for the environment are objects, they will be deep merged. Arrays won't be merged.
+ * If both `defaults` and `value` are objects, they will be deep merged. Arrays won't be merged. Mutations to the returned object won't have an effect over `defaults`.
  */
-export function merge<T>(defaults: any, value: T): T {
-  return mergewith({ data: defaults }, { data: value }, (obj: any, src: any) =>
-    Array.isArray(src) || Array.isArray(obj) ? src : undefined
+export function merge<T, D>(defaults: D, value: T): T | (T & D) {
+  return mergewith(
+    {},
+    { data: defaults },
+    { data: value },
+    (obj: any, src: any) =>
+      Array.isArray(src) || Array.isArray(obj) ? src : undefined
   ).data;
 }
 
 /**
- * If both the default value and the value for the environment are objects, they will be deep merged. Arrays will be merged using concatenation.
+ * If both the `defaults` and `value` are objects, they will be deep merged. Arrays will be concatenated. Mutations to the returned object won't have an effect over `defaults`.
  */
-export function deep<T>(defaults: any, value: T): T {
+export function deep<T, D>(defaults: D, value: T): T | (T & D) {
   return mergewith(
+    {},
     { data: defaults },
     { data: value },
     (obj: any, src: any) => {
