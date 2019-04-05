9a4f8672c58a5e3854bc1ab3e49276593e17f2f9
Rafa Mel <melrcm@gmx.net>
2019-04-05 13:40:45 +0200

    test(fallback): adds tests for fallback

---
+++ test/fallback.test.ts
@@ -0,0 +1,24 @@
+import fallback from '~/fallback';
+
+describe(`shallow`, () => {
+  test(`succeeds wo/ values`, () => {
+    expect(fallback('foo')()).toBe('foo');
+    expect(fallback('foo')('bar')).toBe('bar');
+  });
+  test(`succeeds w/ values`, () => {
+    expect(fallback('foo', ['bar', 'baz'])()).toBe('foo');
+    expect(fallback('foo', ['bar', undefined])()).toBe(undefined);
+    expect(fallback('foo', ['bar', 'baz'])('foobar')).toBe('foo');
+    expect(fallback('foo', ['bar', 'baz'])('bar')).toBe('bar');
+    expect(fallback({ a: 1 }, [{ b: 1 }])({ b: 1 })).toEqual({ a: 1 });
+  });
+});
+describe(`deep`, () => {
+  test(`suceeds`, () => {
+    expect(fallback('foo', ['bar', 'baz'], true)()).toBe('foo');
+    expect(fallback('foo', ['bar', undefined], true)()).toBe(undefined);
+    expect(fallback('foo', ['bar', 'baz'], true)('foobar')).toBe('foo');
+    expect(fallback('foo', ['bar', 'baz'], true)('bar')).toBe('bar');
+    expect(fallback({ a: 1 }, [{ b: 1 }], true)({ b: 1 })).toEqual({ b: 1 });
+  });
+});
